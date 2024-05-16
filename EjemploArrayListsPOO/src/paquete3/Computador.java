3f573da372bf02475350af5e709c649d22e446f6
Juan Villamagua <juanvillamagua3@gmail.com>
2024-05-16 17:52:40 -0500

    Cambios

--- EjemploArrayListsPOO/src/paquete3/Computador.java
+++ EjemploArrayListsPOO/src/paquete3/Computador.java
@@ -9,8 +9,53 @@ package paquete3;
  * @author reroes
  */
 public class Computador {
-   private String marca;
-   private Procesador procesador;
-   private Memoria memoria;
-   private double costoComputador;
+
+    private String marca;
+    private Procesador procesador;
+    private Memoria memoria;
+    private double costoComputador;
+
+    public Computador(String n, double c, Procesador p, Memoria m) {
+        marca = n;
+        procesador = p;
+        memoria = m;
+    }
+
+    public Computador(double c, Procesador p, Memoria m) {
+        marca = "Dell";
+        procesador = p;
+        memoria = m;
+    }
+
+    public void establecerMarca(String n) {
+        marca = n;
+    }
+
+    public void establecerProcesador(Procesador n) {
+        procesador = n;
+    }
+
+    public void establecerMemoria(Memoria c) {
+        memoria = c;
+    }
+
+    public void calcularCostoComputador(){
+        costoComputador = procesador + memoria; 
+    }
+    
+    public String obtenerMarca() {
+        return marca;
+    }
+
+    public Procesador obtenerProcesador() {
+        return procesador;
+    }
+    
+    public Memoria obtenerMemoria(){
+        return memoria;
+    }
+    
+    public double obtenerCostoComputador(){
+        return costoComputador;
+    }
 }
--- EjemploArrayListsPOO/src/paquete3/Memoria.java
+++ EjemploArrayListsPOO/src/paquete3/Memoria.java
@@ -9,6 +9,34 @@ package paquete3;
  * @author reroes
  */
 public class Memoria {
+
     private String marca;
     private double costo;
+
+    public Memoria(String n, double c) {
+        marca = n;
+        costo = c;
+    }
+
+    public Memoria(String n) {
+        marca = n;
+        costo = 350;
+    }
+
+    public void establecerMarca(String n) {
+        marca = n;
+    }
+
+    public void establecerCosto(double c) {
+        costo = c;
+    }
+
+    public String obtenerMarca() {
+        return marca;
+    }
+
+    public double obtenerCosto() {
+        return costo;
+    }
+
 }
